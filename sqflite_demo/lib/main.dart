e0f5ca9a5079e4d566270426d7b1b4c09dca1f5b
Rakesh Kasibhatla <rkasibha@cisco.com>
2018-12-29 15:11:31 +0530

    Create new file for items list page

--- sqflite_demo/lib/main.dart
+++ sqflite_demo/lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite_demo/model/item.dart';
-import 'package:sqflite_demo/database/dbhelper.dart';
 import 'package:sqflite_demo/shopping_list.dart';
 import 'package:sqflite_demo/list_items.dart';
+import 'package:sqflite_demo/items_list.dart';
 
 void main() => runApp(MyApp());
-
-Future<List<Item>> fetchItemsFromDatabase() async {
-  var dbHelper = DBHelper();
-  return dbHelper.getItems();
-}
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,60 +11,8 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       title: 'Database Demo',
-      home: ShoppingListsPage()
+      home: ItemListPage()
     );
   }
 }
 
-class ItemListPage extends StatefulWidget {
-  @override
-  ItemListState createState() => ItemListState();
-}
-
-class ItemListState extends State<ItemListPage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Items'),
-      ),
-      body: Container(
-          padding: EdgeInsets.all(12.0),
-          child: FutureBuilder<List<Item>> (
-              future: fetchItemsFromDatabase(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            ListTile(
-                              leading: CircleAvatar(
-                                child: Text(snapshot.data[index].name[0]),
-                              ),
-                              title: Text(snapshot.data[index].name, style: TextStyle(fontSize: 16.0),),
-                            ),
-                            Divider()
-                          ],
-                        );
-                      }
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return Container(alignment: AlignmentDirectional.center,child: CircularProgressIndicator());
-              }
-          )
-      ),
-    );
-  }
-}
